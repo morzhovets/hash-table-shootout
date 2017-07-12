0a443e314a9ea4e42fbbe1c32c6a68c38e89ebab
Dmitry Bochkarev <morzhovets@gmail.com>
2017-07-12 21:32:46 +0500

    Tree tests

---
+++ src/std_map.cc
@@ -0,0 +1,21 @@
+#include <inttypes.h>
+#include <map>
+#include <string>
+
+typedef std::map<int64_t, int64_t> map_t;
+typedef std::map<std::string, int64_t> str_map_t;
+
+#define SETUP map_t map; str_map_t str_map;
+
+#define INSERT_INT(key, value) map.insert(map_t::value_type(key, value))
+#define DELETE_INT(key) map.erase(key)
+#define FIND_INT_EXISTING(key) if(map.find(key) == map.end()) { printf("error"); exit(1); }
+//#define FIND_INT_MISSING(key) if(map.find(key) != map.end()) { printf("error"); exit(2); }
+#define CHECK_INT_VALUES(value) for(auto&& ref : map) if(ref.second != value) { printf("error"); exit(3); }
+
+#define INSERT_STR(key, value) str_map.insert(str_map_t::value_type(key, value))
+#define DELETE_STR(key) str_map.erase(key);
+#define FIND_STR_EXISTING(key) if(str_map.find(key) == str_map.end()) { printf("error"); exit(4); }
+//#define FIND_STR_MISSING(key) if(str_map.find(key) != str_map.end()) { printf("error"); exit(5); }
+
+#include "template.c"
