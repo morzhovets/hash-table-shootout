ee9fe682a5f99dcd8cdc2187f727a835eb63a94c
Dmitry Bochkarev <morzhovets@gmail.com>
2019-04-06 17:37:01 +0500

    DataTable tests

--- src/momo_data_table_static.cc
+++ src/momo_data_table_static.cc
@@ -11,8 +11,8 @@ struct StructInt
 MOMO_DATA_COLUMN_STRUCT(StructInt, cIntKey);
 MOMO_DATA_COLUMN_STRUCT(StructInt, cIValue);
 
-typedef momo::experimental::DataColumnListStatic<StructInt, momo::MemManagerDefault, momo::experimental::DataSettings<false>> DataColumnsInt;
-typedef momo::experimental::DataTable<DataColumnsInt> DataTableInt;
+typedef momo::DataColumnListStatic<StructInt> DataColumnsInt;
+typedef momo::DataTable<DataColumnsInt> DataTableInt;
 
 struct StructStr
 {
@@ -23,20 +23,20 @@ struct StructStr
 MOMO_DATA_COLUMN_STRUCT(StructStr, cStrKey);
 MOMO_DATA_COLUMN_STRUCT(StructStr, cSValue);
 
-typedef momo::experimental::DataColumnListStatic<StructStr, momo::MemManagerDefault, momo::experimental::DataSettings<false>> DataColumnsStr;
-typedef momo::experimental::DataTable<DataColumnsStr> DataTableStr;
+typedef momo::DataColumnListStatic<StructStr> DataColumnsStr;
+typedef momo::DataTable<DataColumnsStr> DataTableStr;
 
 #define SETUP DataTableInt table; auto ind = table.AddUniqueHashIndex(cIntKey); \
   DataTableStr str_table; auto str_ind = str_table.AddUniqueHashIndex(cStrKey);
 
 #define INSERT_INT(key, value) table.AddRow(cIntKey = key, cIValue = value)
-//#define DELETE_INT(key)
+#define DELETE_INT(key) table.RemoveRow(table.FindByUniqueHash(ind, cIntKey == key)->GetNumber(), false);
 #define FIND_INT_EXISTING(key) if (table.FindByUniqueHash(ind, cIntKey == key).GetCount() != 1) { printf("error"); exit(1); }
 #define FIND_INT_MISSING(key) if (table.FindByUniqueHash(ind, cIntKey == key).GetCount() != 0) { printf("error"); exit(2); }
 #define CHECK_INT_VALUES(value) for (auto rowRef : table) if (rowRef[cIValue] != value) { printf("error"); exit(3); }
 
 #define INSERT_STR(key, value) str_table.AddRow(cStrKey = key, cSValue = value)
-//#define DELETE_STR(key)
+#define DELETE_STR(key) str_table.RemoveRow(str_table.FindByUniqueHash(str_ind, cStrKey == key)->GetNumber(), false);
 #define FIND_STR_EXISTING(key) if (str_table.FindByUniqueHash(str_ind, cStrKey == key).GetCount() != 1) { printf("error"); exit(4); }
 #define FIND_STR_MISSING(key) if (str_table.FindByUniqueHash(str_ind, cStrKey == key).GetCount() != 0) { printf("error"); exit(5); }
 
--- src/momo_data_table_string.cc
+++ src/momo_data_table_string.cc
@@ -6,21 +6,20 @@ MOMO_DATA_COLUMN_STRING(int64_t, cIntKey);
 MOMO_DATA_COLUMN_STRING(std::string, cStrKey);
 MOMO_DATA_COLUMN_STRING(int64_t, cValue);
 
-typedef momo::experimental::DataColumnList<momo::experimental::DataColumnTraits<>,
-  momo::experimental::DataSettings<false>> DataColumns;
-typedef momo::experimental::DataTable<DataColumns> DataTable;
+typedef momo::DataColumnList<> DataColumns;
+typedef momo::DataTable<DataColumns> DataTable;
 
 #define SETUP DataTable table(DataColumns(cIntKey, cValue)); auto ind = table.AddUniqueHashIndex(cIntKey); \
   DataTable str_table(DataColumns(cStrKey, cValue)); auto str_ind = str_table.AddUniqueHashIndex(cStrKey);
 
 #define INSERT_INT(key, value) table.AddRow(cIntKey = key, cValue = value)
-//#define DELETE_INT(key)
+#define DELETE_INT(key) table.RemoveRow(table.FindByUniqueHash(ind, cIntKey == key)->GetNumber(), false);
 #define FIND_INT_EXISTING(key) if (table.FindByUniqueHash(ind, cIntKey == key).GetCount() != 1) { printf("error"); exit(1); }
 #define FIND_INT_MISSING(key) if (table.FindByUniqueHash(ind, cIntKey == key).GetCount() != 0) { printf("error"); exit(2); }
 #define CHECK_INT_VALUES(value) for (auto rowRef : table) if (rowRef[cValue] != value) { printf("error"); exit(3); }
 
 #define INSERT_STR(key, value) str_table.AddRow(cStrKey = key, cValue = value)
-//#define DELETE_STR(key)
+#define DELETE_STR(key) str_table.RemoveRow(str_table.FindByUniqueHash(str_ind, cStrKey == key)->GetNumber(), false);
 #define FIND_STR_EXISTING(key) if (str_table.FindByUniqueHash(str_ind, cStrKey == key).GetCount() != 1) { printf("error"); exit(4); }
 #define FIND_STR_MISSING(key) if (str_table.FindByUniqueHash(str_ind, cStrKey == key).GetCount() != 0) { printf("error"); exit(5); }
 
