384b5758adebd60a720f9e2dc1c6d08d905203d9
Dmitry Bochkarev <morzhovets@gmail.com>
2020-09-30 19:22:46 +0500

    DataTable tests

--- src/momo_data_table_static.cc
+++ src/momo_data_table_static.cc
@@ -30,13 +30,13 @@ typedef momo::DataTable<DataColumnsStr> DataTableStr;
   DataTableStr str_table; auto str_ind = str_table.AddUniqueHashIndex(cStrKey);
 
 #define INSERT_INT(key, value) table.AddRow(cIntKey = key, cIValue = value)
-#define DELETE_INT(key) table.RemoveRow(table.FindByUniqueHash(ind, cIntKey == key)->GetNumber(), false);
+//#define DELETE_INT(key) table.RemoveRow(table.FindByUniqueHash(ind, cIntKey == key)->GetNumber(), false);
 #define FIND_INT_EXISTING(key) if (table.FindByUniqueHash(ind, cIntKey == key).GetCount() != 1) { printf("error"); exit(1); }
 #define FIND_INT_MISSING(key) if (table.FindByUniqueHash(ind, cIntKey == key).GetCount() != 0) { printf("error"); exit(2); }
 #define CHECK_INT_VALUES(value) for (auto rowRef : table) if (rowRef[cIValue] != value) { printf("error"); exit(3); }
 
 #define INSERT_STR(key, value) str_table.AddRow(cStrKey = key, cSValue = value)
-#define DELETE_STR(key) str_table.RemoveRow(str_table.FindByUniqueHash(str_ind, cStrKey == key)->GetNumber(), false);
+//#define DELETE_STR(key) str_table.RemoveRow(str_table.FindByUniqueHash(str_ind, cStrKey == key)->GetNumber(), false);
 #define FIND_STR_EXISTING(key) if (str_table.FindByUniqueHash(str_ind, cStrKey == key).GetCount() != 1) { printf("error"); exit(4); }
 #define FIND_STR_MISSING(key) if (str_table.FindByUniqueHash(str_ind, cStrKey == key).GetCount() != 0) { printf("error"); exit(5); }
 
--- src/momo_data_table_struct.cc
+++ src/momo_data_table_struct.cc
@@ -20,13 +20,13 @@ typedef momo::DataTable<DataColumns> DataTable;
   DataTable str_table(DataColumns(cStrKey, cValue)); auto str_ind = str_table.AddUniqueHashIndex(cStrKey);
 
 #define INSERT_INT(key, value) table.AddRow(cIntKey = key, cValue = value)
-#define DELETE_INT(key) table.RemoveRow(table.FindByUniqueHash(ind, cIntKey == key)->GetNumber(), false);
+//#define DELETE_INT(key) table.RemoveRow(table.FindByUniqueHash(ind, cIntKey == key)->GetNumber(), false);
 #define FIND_INT_EXISTING(key) if (table.FindByUniqueHash(ind, cIntKey == key).GetCount() != 1) { printf("error"); exit(1); }
 #define FIND_INT_MISSING(key) if (table.FindByUniqueHash(ind, cIntKey == key).GetCount() != 0) { printf("error"); exit(2); }
 #define CHECK_INT_VALUES(value) for (auto rowRef : table) if (rowRef[cValue] != value) { printf("error"); exit(3); }
 
 #define INSERT_STR(key, value) str_table.AddRow(cStrKey = key, cValue = value)
-#define DELETE_STR(key) str_table.RemoveRow(str_table.FindByUniqueHash(str_ind, cStrKey == key)->GetNumber(), false);
+//#define DELETE_STR(key) str_table.RemoveRow(str_table.FindByUniqueHash(str_ind, cStrKey == key)->GetNumber(), false);
 #define FIND_STR_EXISTING(key) if (str_table.FindByUniqueHash(str_ind, cStrKey == key).GetCount() != 1) { printf("error"); exit(4); }
 #define FIND_STR_MISSING(key) if (str_table.FindByUniqueHash(str_ind, cStrKey == key).GetCount() != 0) { printf("error"); exit(5); }
 
