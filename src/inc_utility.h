e48b6275b2fdca3d3264d2515b9a237dbeb8adb7
morzhovets <morzhovets@gmail.com>
2026-04-20 23:11:27 +0400

    Simplify utility macros

--- src/inc_utility.h
+++ src/inc_utility.h
@@ -1,30 +1,30 @@
 #include <momo/Version.h>
 #include <momo/Utility.h>
 
-#if MOMO_VERSION_MAJOR == 3 && MOMO_VERSION_MINOR < 12
+#if MOMO_VERSION_MAJOR == 3 && 4 <= MOMO_VERSION_MINOR && MOMO_VERSION_MINOR < 12
 
 #undef MOMO_FRIENDS_SIZE_BEGIN_END_CONST
-#define MOMO_FRIENDS_SIZE_BEGIN_END_CONST(Object, ConstIterator) \
-	friend size_t size(const Object& object) noexcept \
+#define MOMO_FRIENDS_SIZE_BEGIN_END_CONST(Class, ConstIterator) \
+	friend size_t size(const Class& object) noexcept \
 	{ \
 		return object.GetCount(); \
 	} \
-	friend ConstIterator begin(const Object& object) noexcept \
+	friend ConstIterator begin(const Class& object) noexcept \
 	{ \
 		return object.GetBegin(); \
 	} \
-	friend ConstIterator end(const Object& object) noexcept \
+	friend ConstIterator end(const Class& object) noexcept \
 	{ \
 		return object.GetEnd(); \
 	}
 
 #undef MOMO_FRIENDS_BEGIN_END
-#define MOMO_FRIENDS_BEGIN_END(Object, Iterator) \
-	friend Iterator begin(Object& object) noexcept \
+#define MOMO_FRIENDS_BEGIN_END(Class, Iterator) \
+	friend Iterator begin(Class& object) noexcept \
 	{ \
 		return object.GetBegin(); \
 	} \
-	friend Iterator end(Object& object) noexcept \
+	friend Iterator end(Class& object) noexcept \
 	{ \
 		return object.GetEnd(); \
 	}
